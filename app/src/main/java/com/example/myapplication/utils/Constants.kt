ea8f726f57d90ea89029ef81e6c4054a38ff6a61
radekus87 <radekus87@gmail.com>
2021-07-20 13:49:11 +0200

    add unit test

--- app/src/main/java/com/example/myapplication/utils/Constants.kt
+++ app/src/main/java/com/example/myapplication/utils/Constants.kt
@@ -1,25 +1,4 @@
 package com.example.myapplication.utils
 
-object Constants {
-    const val BASE_URL= "https://gpsdevice-38d1e.firebaseio.com/"
-    const val SAMPLE_RESPONSE= "{\n" +
-            "        \"library\": [\n" +
-            "            {\n" +
-            "                \"id\": 1,\n" +
-            "                \"price\": 1.1,\n" +
-            "                \"title\": \"title book 1\"\n" +
-            "            },\n" +
-            "            {\n" +
-            "                \"id\": 2,\n" +
-            "                \"price\": 2.2,\n" +
-            "                \"title\": \"title book 2\"\n" +
-            "            },\n" +
-            "            {\n" +
-            "                \"id\": 3,\n" +
-            "                \"price\": 3.3,\n" +
-            "                \"title\": \"title book 3\"\n" +
-            "            }\n" +
-            "        ]\n" +
-            "    }"
-
+class Constants {
 }
